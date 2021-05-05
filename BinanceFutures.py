344172180ed44fbc82e153a22d33fef2902a8e72
Samuel <mastercraft9090@gmail.com>
2021-05-05 13:37:26 +0300

    Subsequent commits to progress the application along

--- BinanceFutures.py
+++ BinanceFutures.py
@@ -1,5 +1,24 @@
 import logging
+# requests allows to to get, put https responses from a server.
+import requests
+# this library allows us to view json content easily
+import pprint
 
 
 logger = logging.getLogger()
+def get_contracts():
+    response_object = requests.get("https://fapi.binance.com/fapi/v1/exchangeInfo")
+    print(response_object.status_code)
+    #pprint.pprint(response_object.json())
+    #We can print in the response object in the print section but here we will use pretty print
+    #pprint.pprint(response_object.json()['symbols'])
 
+    contracts = []
+
+    for contract in response_object.json()['symbols']:
+        #pprint.pprint(contract)
+        #print(contract['pair'])
+        contracts.append(contract['pair'])
+
+    return contracts
+print(get_contracts())
--- main2.py
+++ main2.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 import logging
-
+from BinanceFutures import get_contracts
 
 logger = logging.getLogger()
 logger.debug("This message is only important when debugging the program")
 logger.info("This just shows basic info")
-logger.warning("This message is about something you should pay attention to")
-logger.error("This message helps debug an error that occurred in your program")
+# logger.warning("This message is about something you should pay attention to")
+# logger.error("This message helps debug an error that occurred in your program")
 
 logger.setLevel(logging.INFO)
 
@@ -20,11 +20,28 @@ file_handler.setFormatter(formatter)
 file_handler.setLevel(logging.DEBUG)
 
 logger.addHandler(stream_handler)
-logger.addHandler((file_handler))
+logger.addHandler(file_handler)
 
 
 
-if __name__ == '__main2__':
+   #if __name__ == '__main2__':
+binance_contracts = get_contracts()
+root = tk.Tk()
+root.configure(bg='gray12')
+i = 0
+j = 0
 
-    root = tk.Tk()
-    root.mainloop()
+calibri_font = ("calibri", 11, "normal")
+for contract in binance_contracts:
+    #label_widget = tk.Label(root, text=contract, borderwidth=1, relief=tk.SOLID, width=13)
+    label_widget = tk.Label(root, text=contract, bg='gray12', fg='SteelBlue1', width=13)
+   # grid() pack()
+    #label_widget.pack(side=tk.LEFT)
+    label_widget.grid(row=i, column=j, sticky='we')
+
+    if i == 10:
+        j += 1
+        i = 0
+    else:
+       i += 1
+root.mainloop()
