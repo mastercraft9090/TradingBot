3774a4949a7e9d195175609605d15aa20eda13f6
Samuel <mastercraft9090@gmail.com>
2021-05-05 05:13:41 +0300

    The first commits of this project

---
+++ BinanceFutures.py
@@ -0,0 +1,6 @@
+import logging
+
+logger = logging.getLogger()
+
+def write_log():
+    logger.info('Hello from the Binance connector ')
---
+++ main2.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+import logging
+from BinanceFutures import write_log
+
+logger = logging.getLogger()
+logger.debug("This message is only important when debugging the program")
+logger.info("This just shows basic info")
+logger.warning("This message is about something you should pay attention to")
+logger.error("This message helps debug an error that occurred in your program")
+
+logger.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s :: %(message)s ')
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler((file_handler))
+
+logger.debug("This message is only important when debugging the program")
+logger.info("This just shows basic info")
+logger.warning("This message is about something you should pay attention to")
+logger.error("This message helps debug an error that occurred in your program")
+write_log()
+
+if __name__ == '__main2__':
+    root = tk.Tk()
+    root.mainloop()
